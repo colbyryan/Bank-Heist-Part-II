1434d1721f0696ba0f2e64b54a11b44c2f079d35
Colby Ryan <colbyr.ryan@gmail.com>
2021-12-01 14:19:17 -0600

    first commit

---
+++ Bank.cs
@@ -0,0 +1,11 @@
+namespace BankHeistPartII
+{
+    public class Bank
+    {
+        public int CashOnHand { get; set; }
+        public int AlarmScore { get; set; }
+        public int VaultScore { get; set; }
+        public int SecurityGuardScore { get; set; }
+        private bool IsSecure = false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankHeistPartII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
